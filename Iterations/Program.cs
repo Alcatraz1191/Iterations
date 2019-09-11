e93cec3c1a48cd64a40e80b555ef582fe4cd03fd
Shreya Gadikar <sggadikar@usf.edu>
2019-09-10 20:51:26 -0400

    Add project files.

---
+++ Iterations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have in professional programming?");
+            string exp = Console.ReadLine();
+            for(int i = 0; i < int.Parse(exp) ; i++)
+            {
+                Console.WriteLine("You have " + (i+1) + " years of experience!");
+            }
+        }
+    }
+}
